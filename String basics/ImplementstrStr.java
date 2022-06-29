bfc9a16d72ed9e17d3665cefd768e90cee3428e2
meet chanchad <meetchanchad8581@gmail.com>
2022-06-28 20:01:54 -0500

    Implement strStr

---
+++ String basics/ImplementstrStr.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        for (int i=0; i <= haystack.length() - needle.length(); i++){
+             if(needle.equals(haystack.substring(i,i+needle.length()))){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
